201f6483beb3b377c191c0ed11051c1c078f73c7
“banzak” <leosantanabr@gmail.com>
2021-10-18 22:59:04 -0300

    filtrar informações recebidas com o uso de uma interface ts

--- node-heat/src/services/AuthenticateUserService.ts
+++ node-heat/src/services/AuthenticateUserService.ts
@@ -5,13 +5,23 @@ import axios from 'axios';
 /**
  * Receber o code(string)
  * Recuperar o access.token no github
+ * Recuperar infos do user no gituhb
  * Verificar se o usuário existe no banco de dados
  * ----- SIM = Gera um token
  * ----- NAO = Cria no DB, gera um token
  * Retorna o token com as infos do user
  */
 
+interface IAccessTokenResponse {
+    access_token: string
+}
 
+interface IUserResponse {
+    avatar_url: string,
+    login: string,
+    id: number,
+    name: string
+}
 
 class AuthenticateUserService {
     // Função para receber o código da autenticação
@@ -19,7 +29,7 @@ class AuthenticateUserService {
         const url = "https://github.com/login/oauth/access_token";
 
         // Chamada para enviar o parametros do acesso
-        const response = await axios.post(url, null, {
+        const { data: accessTokenResponse } = await axios.post<IAccessTokenResponse>(url, null, {
             params: {
                 client_id: process.env.GITHUB_CLIENT_ID,
                 client_secret: process.env.GITHUB_CLIENT_SECRET,
@@ -28,6 +38,13 @@ class AuthenticateUserService {
             headers: {
                 "Accept": "application/json"
             }
+        });
+
+        // Chamada para recuperar infos do user no gituhb
+        const response = await axios.get<IUserResponse>("https://api.github.com/user", {
+            headers: {
+                authorization: `Bearer ${accessTokenResponse.access_token}`
+            },
         });
 
         return response.data;

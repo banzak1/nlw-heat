a0704f4e1b74157c0ac3db4133e8139454e2df7b
Banzak-Uninter <leosantanabr@gmail.com>
2021-10-20 18:10:08 -0300

    Criação e estilização da loginbox

--- web/src/components/LoginBox/index.tsx
+++ web/src/components/LoginBox/index.tsx
@@ -1,7 +1,16 @@
-import styles from './styles.module.scss'
+import {VscGithubInverted} from 'react-icons/vsc';
+
+import styles from './styles.module.scss';
 
 export function LoginBox() {
     return (
-        <div className={styles.loginBoxWrapper}></div>
+        <div className={styles.loginBoxWrapper}>
+            <strong>Entre e compartilhe sua mensagem</strong>
+            <a href="#" className={styles.signInWithGithub}>
+                <VscGithubInverted size="24"/>
+                Entrar com Github
+            </a>
+
+        </div>
     )
 }

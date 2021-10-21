fd4142ef18a9a8fc4fcc2df398b443b43f53f627
Banzak <leosantanabr@gmail.com>
2021-10-21 11:13:07 -0300

    criando função para pegar o codigo do usuario atravez do link gerado pelo github

--- web/src/components/LoginBox/index.tsx
+++ web/src/components/LoginBox/index.tsx
@@ -1,16 +1,31 @@
-import {VscGithubInverted} from 'react-icons/vsc';
+import { useEffect } from 'react';
+import { VscGithubInverted } from 'react-icons/vsc';
 
 import styles from './styles.module.scss';
 
 export function LoginBox() {
+    const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=a4e3286b26e8fe6ea5e1`;
+
+    useEffect(() => {
+        const url = window.location.href;
+        const hasGithubCode = url.includes('?code=');
+
+        if (hasGithubCode) {
+            const [urlWithoutCode, githubCode] = url.split('?code=');
+
+            console.log({ urlWithoutCode, githubCode });
+
+            window.history.pushState({}, '', urlWithoutCode);
+        }
+    }, [])
+
     return (
         <div className={styles.loginBoxWrapper}>
             <strong>Entre e compartilhe sua mensagem</strong>
-            <a href="#" className={styles.signInWithGithub}>
-                <VscGithubInverted size="24"/>
+            <a href={signInUrl} className={styles.signInWithGithub}>
+                <VscGithubInverted size="24" />
                 Entrar com Github
             </a>
-
         </div>
     )
 }

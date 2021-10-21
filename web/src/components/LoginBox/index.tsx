9c26aaea5c94c8e6c4f255639b6e6596f133d3a9
Banzak <leosantanabr@gmail.com>
2021-10-21 11:22:04 -0300

    finalizando requisição teste do usuario

--- web/src/components/LoginBox/index.tsx
+++ web/src/components/LoginBox/index.tsx
@@ -1,10 +1,36 @@
 import { useEffect } from 'react';
 import { VscGithubInverted } from 'react-icons/vsc';
+import { api } from '../../services/api';
 
 import styles from './styles.module.scss';
 
+type AuthResponse = {
+    token: string,
+    user: {
+        id: string;
+        avatar_url: string;
+        name: string;
+        login: string;
+    }
+}
+
+
+
 export function LoginBox() {
     const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=a4e3286b26e8fe6ea5e1`;
+
+    async function signIn(githubCode: string) {
+        const response = await api.post<AuthResponse>('authenticate', {
+            code: githubCode,
+        })
+
+        const { token, user } = response.data;
+
+        localStorage.setItem('@dowhile:token', token);
+
+        console.log(user);
+    }
+
 
     useEffect(() => {
         const url = window.location.href;
@@ -16,6 +42,8 @@ export function LoginBox() {
             console.log({ urlWithoutCode, githubCode });
 
             window.history.pushState({}, '', urlWithoutCode);
+
+            signIn(githubCode);
         }
     }, [])
 

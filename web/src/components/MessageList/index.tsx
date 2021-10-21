e727b93494630b6e8906763ea31a8dd3eb35dbfd
Banzak <leosantanabr@gmail.com>
2021-10-21 09:38:51 -0300

    estilização da página principal

--- web/src/components/MessageList/index.tsx
+++ web/src/components/MessageList/index.tsx
@@ -1,5 +1,42 @@
+import styles from './styles.module.scss';
+
+import logoImg from '../../assets/logo.svg';
+
 export function MessageList() {
     return (
-        <h1>MessageList </h1>
+        <div className={styles.messageListWrapper}>
+            <img src={logoImg} alt="DoWhile-2021" />
+
+            <ul className={styles.messageList}>
+                <li className={styles.message}>
+                    <p className={styles.messageContent}>Não vejo a hora de começar</p>
+                    <div className={styles.messageUser}>
+                        <div className={styles.userImage}>
+                            <img src="https://github.com/banzak1.png" alt="Banzak" />
+                        </div>
+                        <span>Banzak</span>
+                    </div>
+                </li>
+                <li className={styles.message}>
+                    <p className={styles.messageContent}>Não vejo a hora de começar</p>
+                    <div className={styles.messageUser}>
+                        <div className={styles.userImage}>
+                            <img src="https://github.com/banzak1.png" alt="Banzak" />
+                        </div>
+                        <span>Banzak</span>
+                    </div>
+                </li>
+                <li className={styles.message}>
+                    <p className={styles.messageContent}>Não vejo a hora de começar</p>
+                    <div className={styles.messageUser}>
+                        <div className={styles.userImage}>
+                            <img src="https://github.com/banzak1.png" alt="Banzak" />
+                        </div>
+                        <span>Banzak</span>
+                    </div>
+                </li>
+
+            </ul>
+        </div>
     )
 }

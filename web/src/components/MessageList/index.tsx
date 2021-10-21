8fdd95bb024c2db6bfb61b2065c24112b3578052
Banzak <leosantanabr@gmail.com>
2021-10-21 10:37:34 -0300

    Finalizando função para mostrar mensagens em tela

--- web/src/components/MessageList/index.tsx
+++ web/src/components/MessageList/index.tsx
@@ -5,12 +5,21 @@ import logoImg from '../../assets/logo.svg';
 import { api } from '../../services/api';
 import { useEffect, useState } from 'react';
 
+type Message = {
+    id: string,
+    text: string,
+    user: {
+        name: string,
+        avatar_url: string;
+    }
+}
+
 export function MessageList() {
-    const [messages] = useState([]);
+    const [messages, setMessage] = useState<Message[]>([]);
 
     useEffect(() => {
-        api.get("messages/last3").then(response => {
-            console.log(response.data);
+        api.get<Message[]>("messages/last3").then(response => {
+            setMessage(response.data);
         })
     }, [])
 
@@ -19,33 +28,22 @@ export function MessageList() {
             <img src={logoImg} alt="DoWhile-2021" />
 
             <ul className={styles.messageList}>
-                <li className={styles.message}>
-                    <p className={styles.messageContent}>Não vejo a hora de começar</p>
-                    <div className={styles.messageUser}>
-                        <div className={styles.userImage}>
-                            <img src="https://github.com/banzak1.png" alt="Banzak" />
-                        </div>
-                        <span>Banzak</span>
-                    </div>
-                </li>
-                <li className={styles.message}>
-                    <p className={styles.messageContent}>Não vejo a hora de começar</p>
-                    <div className={styles.messageUser}>
-                        <div className={styles.userImage}>
-                            <img src="https://github.com/banzak1.png" alt="Banzak" />
-                        </div>
-                        <span>Banzak</span>
-                    </div>
-                </li>
-                <li className={styles.message}>
-                    <p className={styles.messageContent}>Não vejo a hora de começar</p>
-                    <div className={styles.messageUser}>
-                        <div className={styles.userImage}>
-                            <img src="https://github.com/banzak1.png" alt="Banzak" />
-                        </div>
-                        <span>Banzak</span>
-                    </div>
-                </li>
+
+                {messages.map(message => {
+                    return (
+                        <li key={message.id} className={styles.message}>
+                            <p className={styles.messageContent}>{message.text}</p>
+                            <div className={styles.messageUser}>
+                                <div className={styles.userImage}>
+                                    <img src={message.user.avatar_url} alt={message.user.name} />
+                                </div>
+                                <span>{message.user.name}</span>
+                            </div>
+                        </li>
+                    )
+                })}
+
+
 
             </ul>
         </div>
